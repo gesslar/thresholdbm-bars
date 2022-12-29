7177751d1b738e5234564eba898a64b9702407c8
gesslar <karahd@gmail.com>
2022-12-29 18:50:33 -0500

    update to drawing vitals

--- main.lua
+++ main.lua
@@ -17,6 +17,38 @@ ThresholdBM.Dots = ThresholdBM.Dots or {
     "░", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", "▉", "█",
 }
 
+function ThresholdBM.CenterText(text, width)
+    local mid = math.floor(width / 2)
+    local text_len = string.len(text)
+    local text_mid = math.floor(text_len / 2)
+    local spaces = mid - text_mid
+    local remain = width - spaces - text_len
+
+    return string.rep(" ", spaces) .. text .. string.rep(" ", remain)
+end
+
+function ThresholdBM.DetermineBarWidth()
+    local width, _ = blight.terminal_dimensions()
+
+    width = width - 7
+
+    local bar_width = (math.floor(width / 3)) - 2
+    local remainder = width % 3
+    local hp_total_width = bar_width
+    local sp_total_width = bar_width
+    local ep_total_width = bar_width
+
+    if remainder then
+        sp_total_width = sp_total_width + 1
+        remainder = remainder - 1
+    end
+    if remainder then
+        ep_total_width = ep_total_width + 1
+    end
+print(hp_total_width, sp_total_width, ep_total_width)
+    return hp_total_width, sp_total_width, ep_total_width
+end
+
 function ThresholdBM.DrawVitals()
     local hp    = ThresholdBM.Vitals["hp"] ;
     local hpmax = ThresholdBM.Vitals["maxhp"] ;
@@ -34,11 +66,27 @@ function ThresholdBM.DrawVitals()
         epper = math.floor(ep / epmax * 100) ;
     end
 
-    blight.status_line(0,
-        "HP: [ "..tostring(hp).."/"..tostring(hpmax).." ("..tostring(hpper).."%) ] " ..
-        "SP: [ "..tostring(sp).."/"..tostring(spmax).." ("..tostring(spper).."%) ] " ..
-        "EP: [ "..tostring(ep).."/"..tostring(epmax).." ("..tostring(epper).."%) ] " ..
-    "")
+    local   hp_total_width,
+    sp_total_width,
+    ep_total_width = ThresholdBM.DetermineBarWidth()
+
+    local hp_inner_width = hp_total_width - 6
+    local sp_inner_width = sp_total_width - 6
+    local ep_inner_width = ep_total_width - 6
+print(hp_inner_width, sp_inner_width, ep_inner_width)
+    local hp_bar =
+        string.format(".HP: [%s]  ", ThresholdBM.CenterText(string.format("%d/%d %.0f%%", hp, hpmax, hpper), hp_inner_width))
+    local sp_bar =
+        string.format("SP: [%s]  ", ThresholdBM.CenterText(string.format("%d/%d %.0f%%", sp, spmax, spper), sp_inner_width))
+    local ep_bar =
+        string.format("EP: [%s]  ", ThresholdBM.CenterText(string.format("%d/%d %.0f%%", ep, epmax, epper), ep_inner_width))
+
+    blight.status_line(0, hp_bar .. " " .. sp_bar .. " " .. ep_bar)
+    -- blight.status_line(0,
+    --     "HP: [ "..tostring(hp).."/"..tostring(hpmax).." ("..tostring(hpper).."%) ] " ..
+    --     "SP: [ "..tostring(sp).."/"..tostring(spmax).." ("..tostring(spper).."%) ] " ..
+    --     "EP: [ "..tostring(ep).."/"..tostring(epmax).." ("..tostring(epper).."%) ] " ..
+    -- "")
 end
 
 function ThresholdBM.DrawBars(width, height)
@@ -50,23 +98,9 @@ function ThresholdBM.DrawBars(width, height)
     local epmax = ThresholdBM.Vitals["maxep"] ;
 
     if hp and sp and ep and hpmax and spmax and epmax then
-        if width == nil or height == nil then
-            width, height = blight.terminal_dimensions()
-        end
-        width = width - 7
-        local bar_width = (math.floor(width / 3)) - 2
-        local remainder = width % 3
-        local hp_total_width = bar_width
-        local sp_total_width = bar_width
-        local ep_total_width = bar_width
-
-        if remainder then
-            sp_total_width = sp_total_width + 1
-            remainder = remainder - 1
-        end
-        if remainder then
-            ep_total_width = ep_total_width + 1
-        end
+        local   hp_total_width,
+                sp_total_width,
+                ep_total_width = ThresholdBM.DetermineBarWidth()
 
         local hpper = math.floor(hp / hpmax * 100) ;
         local spper = math.floor(sp / spmax * 100) ;
@@ -108,16 +142,12 @@ function ThresholdBM.CharVitals(data)
     ThresholdBM.DrawBars()
 end
 
--- function ThresholdBM.CharStatus(data)
---     ThresholdBM.UpdateFoe(data)
---     ThresholdBM.
--- end
-
 gmcp.on_ready(function ()
     ThresholdBM.DrawVitals()
     blight.output("Registering GMCP")
     blight.status_height(2)
     gmcp.register("Char")
     gmcp.receive("Char.Vitals", function(data) ThresholdBM.CharVitals(data) end)
+    blight.on_dimensions_change(function(width, height) ThresholdBM.DrawVitals(width, height) end)
     blight.on_dimensions_change(function(width, height) ThresholdBM.DrawBars(width, height) end)
 end)
